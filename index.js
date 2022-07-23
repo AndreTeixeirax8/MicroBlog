54c8a5d5aa870102c677d488054a9450c013d18c
André Teixeira <andre-x8@hotmail.com>
2022-07-23 10:34:28 -0300

    Configuração inicial do index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// Dependencias do projeto
 const express = require("express");
 const app =express();
+const bodyParser = require("body-parser");
+
+//View engine (Motor de redenrização de html)
+app.set('view engine','ejs');
+
+//Arquivos staticos
+app.use(express.static('public'));
+
+//Ultilizar body parser para aceitar dados de formulario
+app.use(bodyParser.urlencoded({extends:false}));
+//Ultilizar body parser para aceitar dados json
+app.use(bodyParser.json());
+
+
 
 app.get("/",(req,res)=>{
-    res.send("Bemvindo ao microblog")
+    res.render("index");
 })
 
 app.listen(4000,() => {
-    console.log("O servidor está rodando na porta 4000")
+    console.log("O servidor está rodando na porta 4000");
 })
