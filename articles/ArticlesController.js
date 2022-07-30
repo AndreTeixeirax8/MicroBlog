df23716d603e89c5ddc1eaf75e92c7b1cdf20e70
andre.teixeira <andre-x8@hotmail.com>
2022-07-30 09:30:15 -0300

    Iniciado a rota para salvar os artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,6 +2,8 @@ const express = require("express");
 //Necessita dessa variavel visto que vamos chamar rotas apartir daqui alem do inde.js
 const router = express.Router();
 const Category = require("../categories/Category");
+const Articles = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles",(req,res)=>{
 
@@ -14,6 +16,20 @@ router.get("/admin/articles/new",(req,res)=>{
         res.render("admin/articles/new",{categories:categories});
     })
     
+});
+
+router.post("/articles/save",(req,res)=>{
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title:title,
+        slug:slugify(title),
+        body:body,
+        //esse campo é criado do relacionamento entre tabelas seria a chave estrangeira
+        categoryId:category 
+    })
 });
 
 //Exportar essa variavel para link com o arquivo do index.js
