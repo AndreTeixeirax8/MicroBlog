919dbaf847e2a93fa17fd6ace4212ef119e3b965
andre.teixeira <andre-x8@hotmail.com>
2022-07-30 09:17:14 -0300

    Adicionado o select de categoria

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 //Necessita dessa variavel visto que vamos chamar rotas apartir daqui alem do inde.js
 const router = express.Router();
+const Category = require("../categories/Category");
 
 router.get("/articles",(req,res)=>{
 
@@ -9,7 +10,10 @@ res.send("rota Artigos")
 });
 
 router.get("/admin/articles/new",(req,res)=>{
-    res.render("admin/articles/new");
+    Category.findAll().then(categories =>{//Aqui passamos a lista de categorias para a pagina new(view)
+        res.render("admin/articles/new",{categories:categories});
+    })
+    
 });
 
 //Exportar essa variavel para link com o arquivo do index.js
